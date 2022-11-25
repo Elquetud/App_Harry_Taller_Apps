9195fed099e885c3471ccfe6935faa4c9334cd20
Elquetud <jesusromeroarmenta@outlook.com>
2022-11-25 00:29:07 -0700

    Personaje en modelo del proyecto

---
+++ lib/modelo/personaje.dart
@@ -0,0 +1,14 @@
+class Character {
+  final String name;
+  final String house;
+  final bool isStudent;
+  final String actor;
+  final String image;
+
+  Character(
+      {required this.actor,
+      required this.house,
+      required this.image,
+      required this.isStudent,
+      required this.name});
+}
