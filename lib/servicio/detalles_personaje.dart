37711ac62eb6704373e7a1345e8d7ae792931aad
Elquetud <jesusromeroarmenta@outlook.com>
2022-11-30 01:32:34 -0700

    se agrega vista de detalles de personajes

---
+++ lib/servicio/detalles_personaje.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import "../modelo/personaje.dart";
+
+class DetallesPersonaje extends StatefulWidget {
+  final Character character;
+
+  DetallesPersonaje(this.character);
+}
