d82e415ce6a2bcf5149268f4637d483e6c333248
Elquetud <jesusromeroarmenta@outlook.com>
2022-12-01 23:03:10 -0700

    prueba recibiendo personaje de ejemplo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,21 +7,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  test('aceptando valores de personaje', () {
+    var personaje = {
+      "personaje": "Harry Pother",
+      "alive": "true",
+      "score": 1000
+    };
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    images:
+    var parsedJson;
+    personaje.fromJson(parsedJson['personaje']);
   });
 }
